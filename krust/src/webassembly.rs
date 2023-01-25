71425e1cb4f709c8f2e638e5cf3fc75ba8fa96d5
unknown <sebaslogo@gmail.com>
2023-01-25 09:17:47 -0600

    Added struct for serialization

--- krust/src/webassembly.rs
+++ krust/src/webassembly.rs
@@ -5,10 +5,21 @@ extern crate nalgebra as na;
 use na::{Vector3, Matrix4};
 use crate::{solver_gd::IKSolverGD, collision_handler::CollisionHandler, solver_ga::IKSolverGA};
 
-// #[derive(Serialize, Deserialize, Debug)]
-// struct Fields {
-//     origin: Matrix4<f32>
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Fields {
+    origin: Matrix4<f32>,
+    thetas: Vec<f32>,
+    axes: Vec<Vector3<f32>>,
+    radii: Vec<f32>,
+
+    min_angles: Vec<f32>,
+    max_angles: Vec<f32>,
+
+    arm_half_extents: Vec<Vector3<f32>>,
+    arm_offsets: Vec<Vector3<f32>>,
+    world_half_extents: Vec<Vector3<f32>>,
+    world_offsets: Vec<Vector3<f32>>
+}
 
 #[wasm_bindgen]
 extern {
@@ -26,28 +37,19 @@ impl InverseKinematics {
 
     // origin array, angles array, axes array
     // TODO: turn this into a struct
-    pub fn new(origin_str: &str, thetas_str: &str, axes_str: &str, radii_str: &str, min_angles_str: &str, max_angles_str: &str, arm_colliders: &str, arm_offsets: &str, world_colliders: &str, world_offsets: &str) -> InverseKinematics {
+    pub fn new(field_str: &str) -> InverseKinematics {
 
         // TODO: something here to let the user know what failed if we didn't get good values in
-        let origin: Matrix4<f32> = serde_json::from_str(origin_str).unwrap();
-        let thetas: Vec<f32> = serde_json::from_str(thetas_str).unwrap();
-        let axes: Vec<Vector3<f32>> = serde_json::from_str(axes_str).unwrap();
-        let radii: Vec<f32> = serde_json::from_str(radii_str).unwrap();
+        let fields: Fields = serde_json::from_str(field_str).unwrap();
 
-        let min_angles: Vec<f32> = serde_json::from_str(min_angles_str).unwrap();
-        let max_angles: Vec<f32> = serde_json::from_str(max_angles_str).unwrap();
-
-        let arm_collider_vec: Vec<Vector3<f32>> = serde_json::from_str(arm_colliders).unwrap();
-        let arm_offsets_vec: Vec<Vector3<f32>> = serde_json::from_str(arm_offsets).unwrap();
-        let world_collider_vec: Vec<Vector3<f32>> = serde_json::from_str(world_colliders).unwrap();
-        let world_offsets_vec: Vec<Vector3<f32>> = serde_json::from_str(world_offsets).unwrap();
+        // alert(&format!("{:?}", fields));
 
         // let collision_handler: CollisionHandler = CollisionHandler::new(vec![], vec![], vec![]);
-        let collision_handler: CollisionHandler = CollisionHandler::new(arm_collider_vec, world_collider_vec, world_offsets_vec);
+        let collision_handler: CollisionHandler = CollisionHandler::new(fields.arm_half_extents, fields.world_half_extents,fields. world_offsets);
 
         InverseKinematics {
-            ik_solver_gd: IKSolverGD::new(origin, &thetas, &axes, &radii), 
-            ik_solver_ga: IKSolverGA::new(origin, &thetas, &axes, &radii, collision_handler), 
+            ik_solver_gd: IKSolverGD::new(fields.origin, &fields.thetas, &fields.axes, &fields.radii), 
+            ik_solver_ga: IKSolverGA::new(fields.origin, &fields.thetas, &fields.axes, &fields.radii, collision_handler), 
         }
     }
 
