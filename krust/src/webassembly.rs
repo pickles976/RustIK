17ceacccbff38c39f70910980411f9f1d8cd59df
unknown <sebaslogo@gmail.com>
2023-01-25 09:07:38 -0600

    Added serialization of angle constraints

--- krust/src/webassembly.rs
+++ krust/src/webassembly.rs
@@ -5,6 +5,11 @@ extern crate nalgebra as na;
 use na::{Vector3, Matrix4};
 use crate::{solver_gd::IKSolverGD, collision_handler::CollisionHandler, solver_ga::IKSolverGA};
 
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Fields {
+//     origin: Matrix4<f32>
+// }
+
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
@@ -20,13 +25,17 @@ pub struct InverseKinematics {
 impl InverseKinematics {
 
     // origin array, angles array, axes array
-    pub fn new(origin_str: &str, thetas_str: &str, axes_str: &str, radii_str: &str, arm_colliders: &str, arm_offsets: &str, world_colliders: &str, world_offsets: &str) -> InverseKinematics {
+    // TODO: turn this into a struct
+    pub fn new(origin_str: &str, thetas_str: &str, axes_str: &str, radii_str: &str, min_angles_str: &str, max_angles_str: &str, arm_colliders: &str, arm_offsets: &str, world_colliders: &str, world_offsets: &str) -> InverseKinematics {
 
         // TODO: something here to let the user know what failed if we didn't get good values in
         let origin: Matrix4<f32> = serde_json::from_str(origin_str).unwrap();
         let thetas: Vec<f32> = serde_json::from_str(thetas_str).unwrap();
         let axes: Vec<Vector3<f32>> = serde_json::from_str(axes_str).unwrap();
         let radii: Vec<f32> = serde_json::from_str(radii_str).unwrap();
+
+        let min_angles: Vec<f32> = serde_json::from_str(min_angles_str).unwrap();
+        let max_angles: Vec<f32> = serde_json::from_str(max_angles_str).unwrap();
 
         let arm_collider_vec: Vec<Vector3<f32>> = serde_json::from_str(arm_colliders).unwrap();
         let arm_offsets_vec: Vec<Vector3<f32>> = serde_json::from_str(arm_offsets).unwrap();
